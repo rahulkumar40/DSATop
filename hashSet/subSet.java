e9a663d9e7e9cdbe8b14da619c305bbeac4b82c3
rahulkumar40 <rahul4004kumar@gmail.com>
2026-01-14 01:54:09 +0530

    HashSEt T

--- hashSet/subSet.java
+++ hashSet/subSet.java
@@ -1,5 +1,21 @@
 import java.util.*;
 
+/*
+    Question : 
+        Check if an array is subset of another array 
+    
+    description : 
+        Given two arrays a[] and b[] of size m and n respectively, the task is to determine whether b[] is a 
+        subset of a[]. Both arrays are not sorted and element are distinct 
+
+     int arr1[] = { 11, 1, 13, 21, 3, 7 };
+    int arr2[] = { 11, 3, 7, 1 };
+    output -> true
+
+     int arr1[] = { 10,5,2,23,19 };
+    int arr2[] = { 19,5,3 };
+
+*/
 public class subSet {
     static boolean isSubSet(int arr1[], int arr2[]) {
         Set<Integer> set = new HashSet<>();
