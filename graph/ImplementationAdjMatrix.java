052e3628f2149d8e0bc208ca393c485fd747df03
rahulkumar40 <rahul4004kumar@gmail.com>
2026-01-23 22:17:54 +0530

    Basic Graph implement  with adjacency Matrix

--- graph/ImplementationAdjMatrix.java
+++ graph/ImplementationAdjMatrix.java
@@ -1,24 +1,64 @@
-class Graph {
-    int vertex ;
-    Graph(int vertex) {
-        this.vertex = vertex;
-    }
-}
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class ImplementationAdjMatrix {
-    static void addEdge(int source, int dest){
-        a[source][dest]=1;
-        a[dest][souce]=1;
-    }
-    static void addEdge(ArrayList<ArrayList<Integer>> adj, int source, int dest){
-        adj.get(source).put(dest);
-        adj.get(dest).put(source);
-    }
-    public static void main(String[] args) {
-        int v = 5;
-        int e = 10;
-        int a[][] = new int[v+1][e+1];
 
-        
+    /*
+     * 📌 ADJACENCY MATRIX — COMPLETE GUIDE (JAVA)
+     * 1️⃣ Definition (Interview-Ready)
+     * 
+     * An Adjacency Matrix is a 2D array representation of a graph, where
+     * matrix[i][j] = 1 (or weight) means there is an edge from vertex i to j,
+     * otherwise 0.
+     * 
+     * 0 1 2
+     * 0 0 1 1
+     * 1 1 0 0
+     * 2 1 0 0
+     * 
+     */
+
+    static void displayGraph(int[][] adj) {
+        // Queue<Integer> queue= new LinkedList<>();
+        // queue.add(adj[0][0]);
+        // while (!queue.isEmpty()) {
+        // int front = queue.remove();
+
+        // }
+
+        int n = adj.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(adj[i][j] + " | ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of edge");
+        int n = sc.nextInt();
+        int edges[][] = new int[3][3];
+
+        // first edge or 0 edge connected edges
+        edges[0][0] = 0;
+        edges[0][1] = 1;
+        edges[0][2] = 1;
+
+        // second edge or 1 edge connected edges
+        edges[1][0] = 1;
+        edges[1][1] = 0;
+        edges[1][2] = 0;
+
+        // third edge or 2 edge connected edges
+        edges[2][0] = 1;
+        edges[2][1] = 0;
+        edges[2][2] = 0;
+
+        displayGraph(edges);
+
     }
 }
