c5f971524946684a8c9608bc6e592c529a46469b
rahulkumar40 <rahul4004kumar@gmail.com>
2026-01-24 00:07:39 +0530

    Directed Graph Imp - Adj Matrix

---
+++ graph/DirectedGraphAdj.java
@@ -0,0 +1,26 @@
+public class DirectedGraphAdj {
+    static void addEdge(int edge[][], int u, int v) {
+        edge[u][v] = 1;
+    }
+
+    static void displayGraph(int graph[][]) {
+        int n = graph.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(graph[i][j] + (j == n-1 ? " " : " -> "));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int graph[][] = new int[n][n];
+        addEdge(graph, 0, 1);
+        addEdge(graph, 1, 2);
+        addEdge(graph, 2, 3);
+        // addEdge(graph, 0, 1);
+
+        displayGraph(graph);
+    }
+}
